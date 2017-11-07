3e8c314b5da6094a17b678a9a01edd2a7ed5d58c
samuel thode <samuel.c.thode.mil@mail.mil>
2017-11-07 14:00:06 -0500

    Update abo2.c

--- Insecure/abo/abo2.c
+++ Insecure/abo/abo2.c
@@ -1,9 +1,10 @@
-
 /* abo2.c                                       *
  * specially crafted to feed your brain by gera */
 
 /* This is a tricky example to make you think   *
  * and give you some help on the next one       */
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argv,char **argc) {
 	char buf[256];
