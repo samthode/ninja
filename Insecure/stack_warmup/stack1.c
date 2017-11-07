f08d2c72c97349e92682a151a62706f79a2a00b5
samuel thode <samuel.c.thode.mil@mail.mil>
2017-11-07 13:55:34 -0500

    Update stack1.c

--- Insecure/stack_warmup/stack1.c
+++ Insecure/stack_warmup/stack1.c
@@ -1,11 +1,12 @@
 /* stack1.c                                     *
  * specially crafted to feed your brain by gera */
+#include <stdio.h>
 
 int main() {
 	int cookie;
 	char buf[80];
 
-	printf("buf: %08x cookie: %08x\n", &buf;, &cookie;);
+	printf("buf: %p cookie: %p\n", &buf;, &cookie;);
 	gets(buf);
 
 	if (cookie == 0x41424344)
