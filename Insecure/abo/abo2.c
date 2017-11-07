e7cd734b83a64e8838d06bdebbbd0d3250974b0d
samuel thode <samuel.c.thode.mil@mail.mil>
2017-11-07 12:49:56 -0500

    Add new file

---
+++ Insecure/abo/abo2.c
@@ -0,0 +1,13 @@
+
+/* abo2.c                                       *
+ * specially crafted to feed your brain by gera */
+
+/* This is a tricky example to make you think   *
+ * and give you some help on the next one       */
+
+int main(int argv,char **argc) {
+	char buf[256];
+
+	strcpy(buf,argc[1]);
+	exit(1);
+}
