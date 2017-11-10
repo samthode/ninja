d4167af06c0b80e1da51dafebd99ed89148f3019
samuel thode <samuel.c.thode.mil@mail.mil>
2017-11-10 16:51:36 -0500

    Add new file

---
+++ Insecure/stack_warmup/stack3.c
@@ -0,0 +1,14 @@
+/* stack3.c                                     *
+ * specially crafted to feed your brain by gera */
+#include <stdio.h>
+
+int main() {
+	int cookie;
+	char buf[80];
+
+	printf("buf: %p cookie: %p\n", &buf, &cookie);
+	gets(buf);
+
+	if (cookie == 0x01020005)
+		printf("you win!\n");
+}
