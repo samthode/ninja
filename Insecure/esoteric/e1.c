48bfb9bbd12c450ab481f92f5f4d6d91fa050574
samuel thode <samuel.c.thode.mil@mail.mil>
2017-11-10 17:34:46 -0500

    Add new file

---
+++ Insecure/esoteric/e1.c
@@ -0,0 +1,11 @@
+/* e1.c                                         *
+/* specially crafted to feed your brain by gera */
+
+/* jumpy vfprintf, Batman! */
+
+int main(int argv,char **argc) {
+                      /* Can you do it changing the stack?  */
+                      /* Can you do it without changing it? */
+	printf(argc[1]);
+	while(1);
+}
