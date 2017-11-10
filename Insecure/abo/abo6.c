c449b393742f207de7e8141bbd4eca7c065c9037
samuel thode <samuel.c.thode.mil@mail.mil>
2017-11-10 17:04:37 -0500

    Add new file

---
+++ Insecure/abo/abo6.c
@@ -0,0 +1,15 @@
+
+
+/* abo6.c                                       *
+/* specially crafted to feed your brain by gera */
+
+/* wwwhat'u talkin' about? */
+
+int main(int argv,char **argc) {
+	char *pbuf=malloc(strlen(argc[2])+1);
+	char buf[256];
+
+	strcpy(buf,argc[1]);
+	strcpy(pbuf,argc[2]);
+	while(1);
+}
